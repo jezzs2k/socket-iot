18a091dd616bcf777b7aa17eb8938795d2714a3d
Quan_Nguyen <quan.nguyen.2629@gmail.com>
2021-04-16 17:18:10 +0700

    16/4/2021 fix some issue

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');	
+const bodyParser = require('body-parser');
+const ip = require('ip');
+
 const PORT = 3484;								
  
-const http = require('http');					
-const socketio = require('socket.io');	
- 
-const ip = require('ip');
-const app = http.createServer();
-var io = socketio(app);
+const app = express()
 
-app.listen(PORT);
-console.log("Server run with port: " + ip.address() + ":" + PORT)
- 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+	console.log("Server run with port: " + ip.address() + ":" + PORT)
+});
+
+const io = socketio.listen(server);
+
 io.on('connection', function(socket) {	
     console.log("Connected"); 
+	console.log(socket.id);
 	
+
+	socket.emit('colors-to-app', 'adasda')
+
 	socket.on('atime', (val) => {
 		console.log(val);
 	})
