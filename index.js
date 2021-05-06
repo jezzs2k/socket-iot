f15c262128902a1f6db3286bcbf3f337fa04b2b7
Quan_Nguyen <quan.nguyen.2629@gmail.com>
2021-05-06 23:44:42 +0700

    6/5/2021

--- index.js
+++ index.js
@@ -32,39 +32,37 @@ let green = 0;
 let response = {};
 
 io.on('connection', function(socket) {	
-    console.log("Connected ", socket.id); 
-
-	socket.emit('connect-item', 'ok')
-
+	console.log("Connected ", socket.id); 
 	socket.on('stop-device', (val) => {
 		socket.emit('arduno-stop', 'stop', 'stop');
 	})
 
-	//demo
-	setInterval(() => {
-		if (Math.random() > 0.8) {
-			red ++;
-			response = {color: '#e74c3c', total: red, type: 'red'}
-		}else if (Math.random() <= 0.8 && Math.random() > 0.5) {
-			green ++;
-			response = {color: '#2ed573', total: green, type: 'green'}
-		}else{
-			blue ++;
-			response = {color: '#1e90ff', total: blue, type: 'blue'}
-		}
-			socket.emit('colors-to-app', response)
-		}, 1000);
-
+	socket.on('start-device', ({isStart}) => {
+		socket.emit('arduno-start', 'start', 'start');
+		socket.emit('start-success');
+		
+		setInterval(() => {
+			if (Math.random() > 0.8) {
+				red ++;
+				response = {color: '#e74c3c', total: red, type: 'red'}
+			}else if (Math.random() <= 0.8 && Math.random() > 0.5) {
+				green ++;
+				response = {color: '#2ed573', total: green, type: 'green'}
+			}else{
+				blue ++;
+				response = {color: '#1e90ff', total: blue, type: 'blue'}
+			}
+				socket.emit('colors-to-app', response)
+			}, 1000);
+	})
 
 	socket.on('colors', (val) => {
 		const {colors} = val;
-		
 		
 		if (red !== parseInt(colors.slice(1,3))) {
 			response = {color: '#e74c3c', total: parseInt(colors.slice(1,3)), type: 'red'}
 		}
 
-			
 		if (green !== parseInt(colors.slice(3,5))) {
 			response = {color: '#2ed573', total: parseInt(colors.slice(3,4)), type: 'green'}
 		}
@@ -73,20 +71,13 @@ io.on('connection', function(socket) {
 			response = {color: '#1e90ff', total: parseInt(colors.slice(5,7)), type: 'blue'}
 		}
 
-
 		red = parseInt(colors.slice(1,3));
 		green =  parseInt(colors.slice(3,5));
 		blue =  parseInt(colors.slice(5,7));
 
-
-
 		socket.emit('colors-to-app', {red, green, blue});
 	});
-
-	socket.on('atime', (val) => {
-		console.log(val);
-	});
-
+	
 	socket.on('disconnect', function() {
 		console.log("disconnect");
 	})
